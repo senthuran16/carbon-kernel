7ed013cc0a3cf47af4d528543706bd83b892b4be
Janak Amarasena <janakamarasena@gmail.com>
2020-07-29 11:00:49 +0530

    Fix test failure

--- core/org.wso2.carbon.user.core/src/test/java/org/wso2/carbon/user/core/authman/AdvancedPermissionTreeWithIDTest.java
+++ core/org.wso2.carbon.user.core/src/test/java/org/wso2/carbon/user/core/authman/AdvancedPermissionTreeWithIDTest.java
@@ -28,14 +28,17 @@ import org.wso2.carbon.user.core.UserRealm;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.DefaultRealm;
+import org.wso2.carbon.user.core.common.UserIdResolverCache;
 import org.wso2.carbon.user.core.config.TestRealmConfigBuilder;
-import org.wso2.carbon.user.core.jdbc.JDBCRealmTest;
 import org.wso2.carbon.user.core.util.DatabaseUtil;
-import org.wso2.carbon.utils.ServerConstants;
 import org.wso2.carbon.utils.dbcreator.DatabaseCreator;
+import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
 import java.io.File;
 import java.io.InputStream;
+
+import static org.wso2.carbon.user.core.UserStoreConfigConstants.RESOLVE_USER_ID_FROM_USER_NAME_CACHE_NAME;
+import static org.wso2.carbon.user.core.UserStoreConfigConstants.RESOLVE_USER_NAME_FROM_USER_ID_CACHE_NAME;
 
 public class AdvancedPermissionTreeWithIDTest extends BaseTestCase {
     private UserRealm realm = null;
@@ -48,6 +51,11 @@ public class AdvancedPermissionTreeWithIDTest extends BaseTestCase {
     }
 
     public void testStuff() throws Exception {
+
+        UserIdResolverCache.getInstance()
+                .clear(RESOLVE_USER_ID_FROM_USER_NAME_CACHE_NAME, MultitenantConstants.SUPER_TENANT_ID);
+        UserIdResolverCache.getInstance()
+                .clear(RESOLVE_USER_NAME_FROM_USER_ID_CACHE_NAME, MultitenantConstants.SUPER_TENANT_ID);
         DatabaseUtil.closeDatabasePoolConnection();
         initRealmStuff();
         admin = realm.getUserStoreManager();
