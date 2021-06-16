f4395a47d6b7f0bd8362a061afc7dfcdc3ca160f
thumimku <thumilan.15@cse.mrt.ac.lk>
2021-06-16 20:56:16 +0530

    fix-When group is in sub directory of the group search base (in LDAP) user to group assignment is failing

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/UniqueIDReadWriteLDAPUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/UniqueIDReadWriteLDAPUserStoreManager.java
@@ -84,6 +84,7 @@ public class UniqueIDReadWriteLDAPUserStoreManager extends UniqueIDReadOnlyLDAPU
     protected static final String KRB5_PRINCIPAL_NAME_ATTRIBUTE = "krb5PrincipalName";
     protected static final String KRB5_KEY_VERSION_NUMBER_ATTRIBUTE = "krb5KeyVersionNumber";
     protected static final String EMPTY_ATTRIBUTE_STRING = "";
+    protected static final String EQUAL_SIGN = "=";
     private static final String MULTI_ATTRIBUTE_SEPARATOR_DESCRIPTION =
             "This is the separator for multiple claim " + "values";
 
@@ -1488,7 +1489,7 @@ public class UniqueIDReadWriteLDAPUserStoreManager extends UniqueIDReadOnlyLDAPU
                             String groupDN = null;
                             if (groupResults.hasMore()) {
                                 resultedGroup = groupResults.next();
-                                groupDN = getGroupName(resultedGroup);
+                                groupDN = getGroupName(resultedGroup, searchBase);
                             }
                             if (resultedGroup != null && isUserInRole(userNameDN, resultedGroup)) {
                                 this.modifyUserInRole(userNameDN, groupDN, DirContext.REMOVE_ATTRIBUTE, searchBase);
@@ -1536,7 +1537,7 @@ public class UniqueIDReadWriteLDAPUserStoreManager extends UniqueIDReadOnlyLDAPU
                             String groupDN = null;
                             if (groupResults.hasMore()) {
                                 resultedGroup = groupResults.next();
-                                groupDN = getGroupName(resultedGroup);
+                                groupDN = getGroupName(resultedGroup, searchBase);
                             }
                             if (resultedGroup != null && !isUserInRole(userNameDN, resultedGroup)) {
                                 modifyUserInRole(userNameDN, groupDN, DirContext.ADD_ATTRIBUTE, searchBase);
@@ -1574,15 +1575,25 @@ public class UniqueIDReadWriteLDAPUserStoreManager extends UniqueIDReadOnlyLDAPU
         }
     }
 
-    private String getGroupName(SearchResult resultedGroup) throws NamingException {
+    private String getGroupName(SearchResult resultedGroup, String searchBase) throws NamingException {
 
         Attribute attribute = resultedGroup.getAttributes()
                 .get(realmConfig.getUserStoreProperty(LDAPConstants.GROUP_NAME_ATTRIBUTE));
         if (attribute == null) {
             return resultedGroup.getName();
         } else {
-            String groupNameAttributeValue = (String) attribute.get();
-            return realmConfig.getUserStoreProperty(LDAPConstants.GROUP_NAME_ATTRIBUTE) + "=" + groupNameAttributeValue;
+            // Handling role in sub group directory.
+            String groupDN = StringUtils.replace(resultedGroup.getName(), searchBase, EMPTY_ATTRIBUTE_STRING);
+            if (StringUtils.isNotBlank(groupDN) && StringUtils.endsWith(groupDN, ",")) {
+                groupDN = StringUtils.substring(groupDN,0, -1);
+            }
+            // Handling role in main Group search base.
+            if (StringUtils.split(groupDN, EQUAL_SIGN).length == 2) {
+                String groupNameAttributeValue = (String) attribute.get();
+                return realmConfig.getUserStoreProperty(LDAPConstants.GROUP_NAME_ATTRIBUTE)
+                        + EQUAL_SIGN + groupNameAttributeValue;
+            }
+            return groupDN;
         }
     }
 
@@ -1624,7 +1635,7 @@ public class UniqueIDReadWriteLDAPUserStoreManager extends UniqueIDReadOnlyLDAPU
                 String groupName = null;
                 while (groupSearchResults.hasMoreElements()) {
                     resultedGroup = groupSearchResults.next();
-                    groupName = getGroupName(resultedGroup);
+                    groupName = getGroupName(resultedGroup, searchBase);
                 }
                 // check whether update operations are going to violate non
                 // empty role
